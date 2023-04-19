9e62feea8ea6d34d8fd9fe7aad524f00258e7d7b
Vicentevv <116390036+Vicentevv@users.noreply.github.com>
2023-04-19 11:32:43 -0600

    Add files via upload

--- Jugador.java
+++ Jugador.java
@@ -3,27 +3,60 @@
  * @author Vicente
  */
 public class Jugador {
+    private int cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
 
-    public Jugador(String cedula, String nombre, String apellido, int edad){
+    public Jugador(int cedula, String nombre, String apellido, int edad) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.apellido = apellido;
         this.edad = edad;
     }
-    private String cedula;
-    private String nombre;
-    private String apellido;
-    private int edad;
 
     public void caminar() {
-        System.out.println("Hacer caminar a Alex");
+        System.out.println("El jugador está caminando");
     }
 
     public void correr() {
-        System.out.println("Hacer correr a Alex");
+        System.out.println("El jugador está corriendo");
     }
 
     public void viajar() {
-        System.out.println("Hacer viajar a Alex");
+        System.out.println("El jugador está viajando");
+    }
+
+    // Getters y setters para los atributos
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad=edad;
     }
 }
